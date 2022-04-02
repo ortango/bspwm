30516a01d858167cdb33814e8e82e6d6390deaa1
ortango <8408464+ortango@users.noreply.github.com>
2022-04-02 16:12:48 -0400

    wrong target for is_fullscreen

--- src/tree.c
+++ src/tree.c
@@ -354,7 +354,9 @@ node_t *insert_node(monitor_t *m, desktop_t *d, node_t *n, node_t *f)
 					c->first_child = f;
 					c->second_child = n;
 				}
-				if (p == NULL || single_tiled || (automatic_scheme == SCHEME_LONGEST_SIDE && d->user_layout == LAYOUT_TILED && !IS_FULLSCREEN(c))) {
+				if (p == NULL || single_tiled ||
+				   (automatic_scheme == SCHEME_LONGEST_SIDE && d->user_layout == LAYOUT_TILED &&
+				   (f->client == NULL || !IS_FULLSCREEN(f->client)))) {
 					if (f->rectangle.width > f->rectangle.height) {
 						c->split_type = TYPE_VERTICAL;
 					} else {
