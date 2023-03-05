f2cf1d04eea83ab37241b0fd38601bd0679b2899
ortango <8408464+ortango@users.noreply.github.com>
2023-03-05 17:54:24 -0500

    pushing this to localbuild cause i dont know why it wasnt included in merge. but also why was it in the g != null conditional?

--- src/tree.c
+++ src/tree.c
@@ -1375,7 +1375,6 @@ void unlink_node(monitor_t *m, desktop_t *d, node_t *n)
 		b->parent = g;
 
 		if (g != NULL) {
-			set_collapsed(m, d, b, (p->collapsed));
 			if (is_first_child(p)) {
 				g->first_child = b;
 			} else {
@@ -1384,6 +1383,8 @@ void unlink_node(monitor_t *m, desktop_t *d, node_t *n)
 		} else {
 			d->root = b;
 		}
+
+		set_collapsed(m, d, b, ( b->collapsed || p->collapsed ));
 
 		if (!n->vacant && removal_adjustment) {
 			if (automatic_scheme == SCHEME_SPIRAL) {
