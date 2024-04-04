79933721ef2a8ee1a8d45e006f8063b926ffd798
ortango <8408464+ortango@users.noreply.github.com>
2024-04-03 20:12:25 -0400

    1456isbugged (#3)
    stop triggering report on "fake" geom discrepancy.
    still need to look at motion recorder geometry. are these changes worth it though? xcb_get_geometry and configure_window shouldn't have different values for x and y.

--- src/tree.c
+++ src/tree.c
@@ -122,6 +122,8 @@ void apply_layout(monitor_t *m, desktop_t *d, node_t *n, xcb_rectangle_t rect, x
 		/* floating clients */
 		} else if (s == STATE_FLOATING) {
 			r = n->client->floating_rectangle;
+			r.x -= n->client->border_width;
+			r.y -= n->client->border_width;
 		/* fullscreen clients */
 		} else {
 			r = m->rectangle;
